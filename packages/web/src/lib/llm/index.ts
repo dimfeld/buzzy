d353bf8b00fbb7ac0855740675830840816007df
Daniel Imfeld <daniel@imfeld.dev>
2023-09-11 20:47:10 -1000

    try bun

--- packages/web/src/lib/llm/index.ts
+++ packages/web/src/lib/llm/index.ts
@@ -1,3 +1,5 @@
+import { OPENAI_KEY } from '$env/dynamic/private';
+
 export async function sendChat(chat: string) {
   return `a response to ${chat}`;
 }
--- packages/web/src/routes/+page.svelte
+++ packages/web/src/routes/+page.svelte
@@ -1,33 +1,40 @@
 <script lang="ts">
   import { enhance } from '$app/forms';
-  import { Textarea } from '$lib/components/ui/textarea';
-  let nextChat = '';
+  import Textarea from '$lib/components/ui/textarea/textarea.svelte';
+  import Button from '$lib/components/ui/button/button.svelte';
 
   export let form;
 
   let formEl: HTMLFormElement;
-
-  function submitChat() {
-    nextChat = '';
-  }
 </script>
 
 <main class="flex h-full flex-col">
   <div class="flex-1">Response: {form?.response || ''}</div>
-  <form bind:this={formEl} method="POST" use:enhance>
+  <form
+    bind:this={formEl}
+    class="flex items-stretch gap-2"
+    method="POST"
+    use:enhance={({ formData, cancel }) => {
+      if (!formData.get('chat')) {
+        cancel();
+      }
+    }}
+  >
     <Textarea
       name="chat"
-      bind:value={nextChat}
+      class="flex-1"
+      autofocus
       placeholder="Type your question here"
       on:keydown={(e) => {
         if (e.key === 'Enter' && (e.metaKey || e.ctrlKey)) {
           if (formEl.requestSubmit) {
             formEl.requestSubmit();
           } else {
-            formEl.submit();
+            document.querySelector('#submit-button')?.click();
           }
         }
       }}
     />
+    <Button id="submit-button" type="submit" class="h-auto">Submit</Button>
   </form>
 </main>
